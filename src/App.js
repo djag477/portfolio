186513cf2e94d86f88226ddc9dde96250c958525
djag477 <101579022+djag477@users.noreply.github.com>
2022-07-05 11:43:29 +0200

    revert to mvp hero page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+import Hero from './components/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+      <Hero />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+    <header className="App-header" style={{ height: "30px" }}>
+        
+        <nav>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Work</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+    )
+  
+};
+
+
+
+
+
+export default NavigationBar;
